5d26ade49612cbdf28233da3b5813c6bb0312d63
Wes Mitchell <wmdrums@gmail.com>
2023-09-09 11:40:23 -0500

    updates interface

--- AmazonReviewGenerator.API/Models/Interfaces/IMarkovChainTextGenerator.cs
+++ AmazonReviewGenerator.API/Models/Interfaces/IMarkovChainTextGenerator.cs
@@ -1,7 +1,11 @@
-﻿namespace AmazonReviewGenerator.API.Models.Interfaces
+﻿using System.Collections.Generic;
+
+namespace AmazonReviewGenerator.API.Models.Interfaces
 {
     public interface IMarkovChainTextGenerator
     {
         string GenerateMarkovString();
+
+        Dictionary<string, List<string>> LoadTrainingData(int keySize, string filePath);
     }
 }
